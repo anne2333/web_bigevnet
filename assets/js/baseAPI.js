bf28223f35b88f97b87c6f6b120c815ced32ac7c
shijintao <710780244@qq.com>
2022-07-18 16:59:51 +0800

    完成首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,4 +2,16 @@
 var _URL = 'http://www.liulongbin.top:3007'
 $.ajaxPrefilter(function (options) {
   options.url = _URL + options.url
+  //设置请求头
+  if (options.url.indexOf('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  options.complete = function (res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+      localStorage.removeItem('token')
+      location.href = './login.html'
+    }
+  }
 })
